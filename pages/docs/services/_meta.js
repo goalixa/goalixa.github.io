cd4454158675b2b91fb609d0e7ad8aa319ea5a09
amirreza.rezaie <82rezaeei@gmail.com>
2026-04-06 23:08:25 +0330

    create services documents

---
+++ pages/docs/services/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  "core-api": {
+    title: "Core-API"
+  },
+  "goalixa-auth": {
+    title: "Auth Service"
+  },
+  "goalixa-bff": {
+    title: "BFF Service"
+  },
+  "goalixa-pwa": {
+    title: "PWA App"
+  },
+  "goalixa-landing": {
+    title: "Landing Page"
+  }
+}
