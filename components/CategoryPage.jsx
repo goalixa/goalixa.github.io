082e2926531180e5d3227d3b2b59c90b638534e3
amirreza.rezaie <82rezaeei@gmail.com>
2026-04-28 19:38:19 +0330

    update posts

---
+++ components/CategoryPage.jsx
@@ -0,0 +1,19 @@
+export default function CategoryPage({ title, description, icon, children }) {
+  return (
+    <div className="category-container">
+      <div className="category-grid">
+        {/* Header */}
+        <div className="category-header">
+          {icon && <div className="category-icon">{icon}</div>}
+          <h1 className="category-title">{title}</h1>
+          <p className="category-description">{description}</p>
+        </div>
+
+        {/* Content (article list from MDX) */}
+        <div className="category-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import '../styles/about.css'
 import '../styles/journey.css'
 import '../styles/architecture.css'
 import '../styles/timeline.css'
+import '../styles/category.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
